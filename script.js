7602095806571446a49ca2a3e4b8e246f2b0aa61
Pomegranule <elleg1@gmx.com>
2021-06-14 23:15:25 +0100

    Okay so, I THINK I did this right...
    I started on the JS for making the tabs hide and show themselves when a corresponding button is clicked.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let collapseButtons = document.getElementById("collapseTabButtons");
 collapseButtons.addEventListener("click", showTabButtons);
 
+
+
 function showTabButtons(){
     let tabButtons = document.getElementsByClassName("tabButtons");
-    
     for(let loop=0; loop<tabButtons.length; loop++){
         let displayState = tabButtons[loop].style.display;
         if(displayState === "none"){
@@ -11,5 +12,23 @@ function showTabButtons(){
         } else {
             tabButtons[loop].style.display = "none";
         }
+    }
+}
+
+let homeTabButton = document.getElementById("homeTabButton");
+homeTabButton.addEventListener("click", homeTabClick);
+
+function homeTabClick(){
+    let tabContent = document.getElementsByClassName("tabContent");
+    let homeTabContent = document.getElementById("homeTab");
+    let homeTabContentStatus = homeTabContent.style.display;
+    if(homeTabContentStatus === "none"){
+        //If homeTabContentStatus is equal to "none", cycle through the tabContent class and set them to none;
+        //set "homeTab" to "flex".
+        for(let loop=0; loop<tabContent.length; loop++){
+            let tabContentStatus = tabContent[loop].style.display;
+            tabContentStatus === "none";
+        }
+        homeTabContentStatus === "flex";
     }
 }
