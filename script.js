8958f38c3bde92324932ed44a98abc31301c1ffb
Pomegranule <elleg1@gmx.com>
2021-06-15 12:15:35 +0100

    So, I changed a lot of stuff here. Started the CSS again using a cool tutorial I found. The goal is to have the navbar displayed along the bottom for smaller devices.

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let collapseButtons = document.getElementById("collapseTabButtons");
-collapseButtons.addEventListener("click", showTabButtons);
-
-
-
-function showTabButtons(){
-    let tabButtons = document.getElementsByClassName("tabButtons");
-    for(let loop=0; loop<tabButtons.length; loop++){
-        let displayState = tabButtons[loop].style.display;
-        if(displayState === "none"){
-            tabButtons[loop].style.display = "flex";
-        } else {
-            tabButtons[loop].style.display = "none";
-        }
-    }
-}
-
 let homeTabButton = document.getElementById("homeTabButton");
 homeTabButton.addEventListener("click", homeTabClick);
 
